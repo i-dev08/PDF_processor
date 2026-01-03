3a93a1de3d0628f4d5c2dcc722c67624a5e45c67
i-dev <ishikardev8@gmail.com>
2026-01-03 21:55:29 +0530

    Initial setup complete

---
+++ index.js
@@ -0,0 +1,42 @@
+const cors = require("cors");
+const express = require("express");
+const multer = require("multer");
+const upload = multer({dest:'uploads/'});
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const jobs= {};
+
+app.post("/process",upload.array('files'),(req,res) => {
+    console.log("Recieved file:",req.files.length);
+    const jobid = Date.now().toString();
+    const MAX_SIZE = 5*1024*1024;
+
+    jobs[jobid]={
+        status:"PROCESSING...",
+        files:req.files,
+        result_url:null,
+    };
+
+    setTimeout(() => {
+        if (jobs[jobid].files.some(file => file.size > MAX_SIZE|| (!file.mimetype || file.mimetype !== "application/pdf"))) {
+            jobs[jobid].status="FAILED";
+        } else {
+            jobs[jobid].status="COMPLETED";
+            jobs[jobid].result_url="http://example.com/result.pdf";
+        }
+    },5000);
+
+    res.json({jobid});
+});
+
+app.get("/status/:id",(req,res) => {
+    const job = jobs[req.params.id];
+    if (!job) {return res.status(404).json({ error:"Job not found"});}
+    res.json(job);
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
